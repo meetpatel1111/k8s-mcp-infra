ffe0c6ca929850a33b351b38961e2bf34a027939
Meetkumar Patel <pmeet464@gmail.com>
2026-09-08 22:16:15 +0530

    Update Terraform to 1.17.0 and Azure provider to 4.8.0

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.17.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.8.0"
+  version = ">= 4.8.0"
     }
   }
 }
